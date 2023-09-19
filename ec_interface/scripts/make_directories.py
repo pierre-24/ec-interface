976e01f3885091270f6ea5c3e7b992f91f5bf050
Pierre Beaujean <pierreb24@gmail.com>
2023-09-19 21:25:56 +0200

    copy instead of symlink

--- ec_interface/scripts/make_directories.py
+++ ec_interface/scripts/make_directories.py
@@ -5,6 +5,7 @@ Create directories for calculations
 import argparse
 import pathlib
 import re
+import shutil
 import sys
 
 from ec_interface.ec_parameters import ECParameters
@@ -18,7 +19,7 @@ def getdir(inp: str) -> pathlib.Path:
     return directory
 
 
-def create_input_directories(directory: pathlib.Path):
+def create_input_directories(directory: pathlib.Path, use_symlinks: bool = True):
     """Create directories containing input files, ready to compute
     """
 
@@ -47,7 +48,7 @@ def create_input_directories(directory: pathlib.Path):
     incar_content = incar_file.open().read()
 
     # check content
-    to_find = ['LCHARG', 'LVHAR']
+    to_find = ['LCHARG', 'LVHAR', 'LVTOT']
     for keyword in to_find:
         if not re.compile(r'{}\s*=\s*\.TRUE\.'.format(keyword)).search(incar_content):
             raise Exception('cannot find `{} = .TRUE.` in INCAR'.format(keyword))
@@ -65,19 +66,23 @@ def create_input_directories(directory: pathlib.Path):
             f.write(incar_content)
             f.write('\n! EC calculation\nNELECT = {:.3f}'.format(n))
 
-        # create symlinks for other files
+        # create symlinks/copy other files
         for p in ['POSCAR', 'POTCAR', 'KPOINTS']:
-            (subdirectory / p).symlink_to('../{}'.format(p))
+            if use_symlinks:
+                (subdirectory / p).symlink_to('../{}'.format(p))
+            else:
+                shutil.copy(p, subdirectory / p)
 
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-i', '--directory', default='.', type=getdir, help='Input directory')
+    parser.add_argument('-c', '--copy-files', action='store_true', help='Copy files instead of using symlinks')
     args = parser.parse_args()
 
     # get ec input info
     try:
-        create_input_directories(args.directory)
+        create_input_directories(args.directory, use_symlinks=not args.copy_files)
     except Exception as e:
         print('error:', e, file=sys.stderr)
 
