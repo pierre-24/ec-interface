ac148f80f814309f4ca08cbd5aa51982bb700525
Pierre Beaujean <pierreb24@gmail.com>
2024-01-12 15:26:48 +0100

    Bump version: 0.5.1 → 0.5.2

--- ec_interface/__init__.py
+++ ec_interface/__init__.py
@@ -2,7 +2,7 @@
 A small code to create EC interface calculations with VASP and extract the corresponding data out of them.
 """
 
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 __author__ = 'Pierre Beaujean'
 __maintainer__ = 'Pierre Beaujean'
 __email__ = 'pierre.beaujean@unamur.be'
